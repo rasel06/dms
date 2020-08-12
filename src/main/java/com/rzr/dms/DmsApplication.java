5c7fef3aceed42e062e3bfd400d12e59bf3363d9
rasel <rasel06@gmail.com>
2020-08-12 22:33:29 +0600

    Logout Implemented

--- src/main/java/com/rzr/dms/DmsApplication.java
+++ src/main/java/com/rzr/dms/DmsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DmsApplication.class, args);
+    }
 
 }
--- src/main/java/com/rzr/dms/controllers/SecurityConfig.java
+++ src/main/java/com/rzr/dms/controllers/SecurityConfig.java
@@ -6,31 +6,48 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
         http
-        .authorizeRequests()
-        		.antMatchers("/admin/**").permitAll()
+                .authorizeRequests()
+                .antMatchers("/admin/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-        .formLogin()
-                .loginPage("/login")
-                .permitAll();
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login");
+                //.logoutSuccessUrl("/")
+                //.invalidateHttpSession(true)
+                //.deleteCookies("JSESSIONID");
 
-		/*
-		 * http.authorizeRequests() .antMatchers("/", "/home").permitAll()
-		 * .hasAnyAuthority("USER", "CREATOR", "EDITOR", "ADMIN")
-		 * .antMatchers("/new").hasAnyAuthority("ADMIN", "CREATOR")
-		 * .antMatchers("/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
-		 * .antMatchers("/delete/**").hasAuthority("ADMIN")
-		 * 
-		 * .anyRequest().authenticated() .and() .formLogin().permitAll() .and()
-		 * .logout().permitAll();
-		 */
-		/*
-		 * .and() .exceptionHandling().accessDeniedPage("/403");
-		 */
+//        http
+//        .authorizeRequests()
+//        		.antMatchers("/admin/**").permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//        .formLogin()
+//                .loginPage("/login")
+//                .permitAll();
 
-	}
+        /*
+         * http.authorizeRequests() .antMatchers("/", "/home").permitAll()
+         * .hasAnyAuthority("USER", "CREATOR", "EDITOR", "ADMIN")
+         * .antMatchers("/new").hasAnyAuthority("ADMIN", "CREATOR")
+         * .antMatchers("/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
+         * .antMatchers("/delete/**").hasAuthority("ADMIN")
+         *
+         * .anyRequest().authenticated() .and() .formLogin().permitAll() .and()
+         * .logout().permitAll();
+         */
+        /*
+         * .and() .exceptionHandling().accessDeniedPage("/403");
+         */
+
+    }
 }
